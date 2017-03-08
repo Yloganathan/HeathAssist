2408a64631a54ce387569edae02014fb2e8db613
Yuvarani Loganathan <yuva@apto.com>
2017-03-08 10:27:15 -0700

    Heroku test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userAPI = require('../server/api/user-api');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
- res.render('index', { title: 'Express' });
-});
+router.get('/', userAPI.retrieveUsers);
 
 module.exports = router;
