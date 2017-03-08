0a16decde0bbd9febc7d566defdabed71bd86638
Yuvarani Loganathan <yuva@apto.com>
2017-03-08 11:03:47 -0700

    Convert errors to JSON

--- app.js
+++ app.js
@@ -37,23 +37,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status( err.code || 500 )
+           .json({
+               status: 'error',
+               message: err
+           });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500)
+       .json({
+           status: 'error',
+           message: err.message
+       });
 });
 
 
